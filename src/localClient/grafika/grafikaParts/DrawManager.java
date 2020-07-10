77c02ee21c8b9f180415b3e12bcf2ee56c305af4
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2020-07-10 21:22:27 +0300

    overPanel fix

--- src/localClient/grafika/grafikaParts/DrawManager.java
+++ src/localClient/grafika/grafikaParts/DrawManager.java
@@ -17,6 +17,7 @@ public abstract class DrawManager {
     public PanelL panelL;
     public PanelR panelR;
     public CenterPanel centerPanel;
+    public OverPanel overPanel;
 
     public boolean drawHeader = true, drawFooter = true,
             drawPanelL = true,  drawPanelR = true,
@@ -32,6 +33,7 @@ public abstract class DrawManager {
         panelL = new PanelL(layout, colorPalette.pair2);
         panelR = new PanelR(layout, colorPalette.pair2);
         centerPanel = new CenterPanel(layout, colorPalette.pair3);
+        overPanel = new OverPanel();
     }
 
     public void main(Graphics g, ClientThread thread){
@@ -47,8 +49,7 @@ public abstract class DrawManager {
 
         if(thread.dati.grafikasDati.drawLayoutGrid) drawLayoutGrid(g, thread.dati.grafikasDati.layoutGridColor);
 
-        if(drawOverPanel) drawOverPanel(g, thread, layout);
-
+        if(drawOverPanel) overPanel.draw(g, thread, layout);
     }
 
     private void drawFons(Graphics g, ClientThread thread) {
@@ -86,75 +87,6 @@ public abstract class DrawManager {
         textXY = new int[]{footer.XY[0] + textOffset[0], footer.XY[1] + textOffset[1]};
         g.drawString("PlayerViewFooter", textXY[0], textXY[1]);
 
-    }
-
-    private void drawOverPanel(Graphics g, ClientThread thread, SampleLayout layout){
-
-        if(thread.dati.grafikasDati.drawSampleImages)
-            drawSampleImages(g, thread.dati.grafikasDati.images);
-        if(thread.dati.grafikasDati.drawInputDiagnosticsPanel)
-            drawInputDiagnosticsInfo(g, thread, thread.dati.grafikasDati.colorPalette.pair2[1], layout);
-        if(thread.dati.grafikasDati.drawInputDiagnosticsPanel)
-            drawCalculationTimes(g);
-    }
-
-    private void drawInputDiagnosticsInfo(Graphics g, ClientThread thread, Color textColor, SampleLayout layout) { //ieavades pârbaude un grafiskâ informâcija
-
-        int[] textOffset = {5, 200};
-
-        int x0 = layout.panelLX + textOffset[0],
-                y0 = layout.footerY - textOffset[1],
-                yw = 15, w = 0;
-
-        g.setColor(textColor);
-
-        CalculationTimeCalculator calculationTimeCalculatorTemp = thread.calculationTimeCalculator; //setup loga tehniskie parametri
-        g.drawString("Grafikas parametri:", x0, y0+w*yw); w++;
-        g.drawString("CalcTimeMax("+calculationTimeCalculatorTemp.timeCalculationFrequency+"): "+
-                calculationTimeCalculatorTemp.calculationTimeMax, x0, y0+w*yw); w++;
-        g.drawString("TotalFrameTime("+calculationTimeCalculatorTemp.timeCalculationFrequency+"): "+
-                calculationTimeCalculatorTemp.totalFrameTimeMax, x0, y0+w*yw); w++;
-
-        g.drawString("MinFPS("+calculationTimeCalculatorTemp.timeCalculationFrequency+"): "+
-                (1000/calculationTimeCalculatorTemp.totalFrameTimeMax), x0, y0+w*yw);w++;
-        g.drawString("---------------",x0, y0+w*yw);w++;
-
-
-        g.drawString("x: "+thread.input.xPele+" y: "+thread.input.yPele, x0, y0+w*yw); w++; //peles x un y
-        g.drawString("windowActive: " + thread.windowActive, x0, y0+w*yw); w++;
-
-        //piespiesto pogu izvade (klaviatûrai)
-        String teksts;
-        for (int i=0; i<thread.input.pogas.length; i++) {
-            teksts="poga "+i+" = "+thread.input.pogas[i];
-            g.drawString(teksts, x0, y0+w*yw); w++;
-        }
-
-    }
-
-    private void drawCalculationTimes(Graphics g){
-        //Calculation times display not implemented yed
-    }
-
-    private void drawSampleImages(Graphics g, HashMap<String, Image> images){
-
-        int[] imageXYrelative = {300, 200},
-                imageSize = {150, 150},
-                imageXY = new int[]{imageXYrelative[0] - imageSize[0]/2,
-                        imageXYrelative[1] - imageSize[1]/2};
-        g.drawImage(images.get("zvaigzne"), imageXY[0], imageXY[1], imageSize[0], imageSize[1],null);
-
-        imageXYrelative = new int[]{500, 300};
-        imageSize = new int[]{200, 200};
-        imageXY = new int[]{imageXYrelative[0] - imageSize[0]/2,
-                imageXYrelative[1] - imageSize[1]/2};
-        g.drawImage(images.get("banana"), imageXY[0], imageXY[1], imageSize[0], imageSize[1],null);
-
-        imageXYrelative = new int[]{150, 300};
-        imageSize = new int[]{30, 30};
-        imageXY = new int[]{imageXYrelative[0] - imageSize[0]/2,
-                imageXYrelative[1] - imageSize[1]/2};
-        g.drawImage(images.get("banana"), imageXY[0], imageXY[1], imageSize[0], imageSize[1],null);
     }
 
 }
