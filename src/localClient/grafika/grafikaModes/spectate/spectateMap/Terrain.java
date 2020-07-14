724ae9fe81b91f0c1fba1a053bff4f71959f2e48
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2020-07-14 12:27:28 +0300

    spectator map background fix

--- src/localClient/grafika/grafikaModes/spectate/spectateMap/Terrain.java
+++ src/localClient/grafika/grafikaModes/spectate/spectateMap/Terrain.java
@@ -15,23 +15,39 @@ public class Terrain {
     void draw(Graphics g, Dati dati, int[] contentsXY, int[] contentsSize, int[] drawCenterXY){
         DrawManager.SpectateMapInfo spectateMapInfo = dati.drawManagerList.get(dati.modeCurrent).spectateMapInfo;
 
-        drawLaukumaFons(g, spectateMapInfo, contentsSize, drawCenterXY);
-
-        if (spectateMapInfo.cellGrid) drawCellGrid(g);
-        if (spectateMapInfo.chunkGrid) drawChunkGrid(g);
+        drawLaukumaFons(g, spectateMapInfo, drawCenterXY, contentsXY, contentsSize);
 
         drawTerrain();
+
+        if (spectateMapInfo.chunkGrid || spectateMapInfo.cellGrid) {
+            if (spectateMapInfo.cellGrid) drawCellGrid(g);
+            drawChunkGrid(g);
+            drawLaukumaMala(g, spectateMapInfo, drawCenterXY);
+        }
     }
 
-    private void drawLaukumaFons(Graphics g, DrawManager.SpectateMapInfo spectateMapInfo, int[] contentsSize, int[] drawCenterXY){
-        g.setColor(DataBase.laukumaKrasa); //uzzîmç laukumu
-        g.fillRect(
+    private void drawLaukumaFons(Graphics g, DrawManager.SpectateMapInfo spectateMapInfo,
+                                 int[] drawCenterXY, int[] contentsXY, int[] contentsSize){
+        //uzzîmç laukumu
+        g.setColor(DataBase.laukumaKrasa);
+
+        if(spectateMapInfo.mapWrap) g.fillRect( //viss contents
+                contentsXY[0], contentsXY[1],
+                contentsSize[0], contentsSize[1]);
+        else g.fillRect( //tikai 1 laukums
                 (int)(drawCenterXY[0] - spectateMapInfo.centerXY[0] * spectateMapInfo.merogs),
                 (int)(drawCenterXY[1] - spectateMapInfo.centerXY[1] * spectateMapInfo.merogs),
                 (int)(DataBase.laukumaPlatumsSum * spectateMapInfo.merogs),
                 (int)(DataBase.laukumaAugstumsSum * spectateMapInfo.merogs));
+    }
 
-
+    private void drawLaukumaMala(Graphics g, DrawManager.SpectateMapInfo spectateMapInfo, int[] drawCenterXY){
+        g.setColor(Color.red); //uzzîmç laukumu
+        g.drawRect(
+                (int)(drawCenterXY[0] - spectateMapInfo.centerXY[0] * spectateMapInfo.merogs),
+                (int)(drawCenterXY[1] - spectateMapInfo.centerXY[1] * spectateMapInfo.merogs),
+                (int)(DataBase.laukumaPlatumsSum * spectateMapInfo.merogs),
+                (int)(DataBase.laukumaAugstumsSum * spectateMapInfo.merogs));
     }
 
     private void drawChunkGrid(Graphics g){
