6336c1fb24acaf511436f61a1afc42e45c0dceb1
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2020-07-24 20:47:03 +0300

    minor changes

--- src/Setup.java
+++ src/Setup.java
@@ -4,7 +4,7 @@ public class Setup {
 
     //pati pirmâ klase, kura tâlâk palaiþ pârçjâs
     private static final String nosaukums = "Nabagi",
-            versija = "0.11.9 (beta)";
+            versija = "0.11.10 (beta)";
 
     public static void main(String... args) {
         System.out.println(nosaukums + " (versija: " + versija + ")");
